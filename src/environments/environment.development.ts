96f6cbeb6e1e75d14ecf9b0807994cb995499d51
OscarArise <arisechonova@gmail.com>
2025-06-12 01:51:07 -0600

    Primera fusion de correciones

---
+++ src/environments/environment.development.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  firebase: {
+    apiKey: "AIzaSyBkhZwSwygjJnrE6ntRpekaqe8G1-Z4Ot4",
+    authDomain: "pruebasfirebase-c2278.appspot.com",
+    projectId: "pruebasfirebase-c2278",
+    storageBucket: "pruebasfirebase-c2278.appspot.com",
+    messagingSenderId: "51759880341",
+    appId: "1:51759880341:web:f1a95b08d1d61144b6239c",
+    measurementId: "G-3WMNZRQ91Z"
+  }
+};
