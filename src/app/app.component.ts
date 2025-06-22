d400355f324f712a1a9604ad8ff426e814c97e90
Natsui122 <saulagaspar@gmail.com>
2025-06-21 18:33:49 -0600

    update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,11 +5,22 @@ import { InicioComponent } from './components/inicio/inicio/inicio.component';
 import { FooterComponent } from "./components/footer/footer.component";
 import { HeaderComponent } from "./components/header/header.component";
 import { SuscripcionComponent } from './components/suscripcion/suscripcion.component';
+import { CarritoFlotanteComponent } from '../app/components/carrito-flotante/carrito-flotante/carrito-flotante.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, NavbarComponent, RouterModule, InicioComponent, HeaderComponent, FooterComponent, SuscripcionComponent],
-  template: `<router-outlet />`,
+  imports: [
+    RouterOutlet, 
+    NavbarComponent, 
+    RouterModule, 
+    InicioComponent, 
+    HeaderComponent, 
+    FooterComponent, 
+    SuscripcionComponent,
+    CarritoFlotanteComponent
+  ],
+  template: `<router-outlet />
+             <app-carrito-flotante></app-carrito-flotante>`,
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
